d322becb3ac8300113c29b61f76b5c1750752b8f
Amit Dabra <amitdavre7@gmail.com>
2023-12-20 11:24:41 +0530

    deleted comment 20-12-2023

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -6,7 +6,6 @@
 	<meta name="description" content="The small framework with powerful features">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
-	<!-- edit comment 13-12-2023 -->
 	<!-- STYLES -->
 
 	<style {csp-style-nonce}>
